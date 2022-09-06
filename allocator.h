b0e4723082d6262e9ed71f8f6e36a6ce515a9d70
SRAZKVT <sarah.mathey66666@gmail.com>
2022-09-06 16:32:50 +0200

    added documentation

--- allocator.h
+++ allocator.h
@@ -10,12 +10,36 @@ typedef struct Allocator {
 	} list;
 } Allocator;
 
+/**
+ * Instanciates a new allocator
+ * 
+ * @return pointer to the newly instanciated allocator
+ */
 Allocator *allocator_init();
 
+/**
+ * Uses an instanciated allocator to allocate memory.
+ * Will also add the pointer returned by malloc to the allocator's internal buffer
+ * 
+ * @param alloc the instanciated allocator which should wrap the malloc call
+ * @param size the size of the buffer requested
+ * 
+ * @return pointer to the requested buffer
+ */
 void *allocator_alloc(Allocator *alloc, size_t size);
 
+/**
+ * Free all pointers stored within the allocator, essentially resetting it. Allows to use an allocator in a loop, and have all memory needed within the loop freed by this function. Is implicitely called by allocator_destroy, so call this function before isn't necessary.
+ * 
+ * @param alloc the instanciated allocator to reset
+ */
 void allocator_freeAll(Allocator *alloc);
 
+/**
+ * Frees all pointers stored within the allocator, and destroy the allocator itself.
+ * 
+ * @param alloc the instanciated allocator to destroy
+ */
 void allocator_destroy(Allocator *alloc);
 
 #endif // allocator_h_INCLUDED
